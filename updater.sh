fb523de10705c54c05d6d0124081a75a3956bfca
devDath@wslroot <s_wipulasi17@rhrk.uni-kl.de>
2021-09-17 16:56:39 +0200

    added functions to return and update dns record

--- updater.sh
+++ updater.sh
@@ -9,31 +9,54 @@ curl_param="X-API-KEY:"
 dns_zone="dns/v1/zones"
 dns_records_start="dns/v1/"
 dns_records_end="/records/"
+zone="zones/"
 output_type="accept: application/json"
 
 function checkIfZoneIdExists {
+
 	if [ -z ${zone_id+x} ]; 
 	then  
 		# get zone ID
-		zone_id=$(curl -X GET "$base_url$dns_zone" -H "$curl_param $api_key" | jq '.[] | .id?');
+		zone_id=$(curl -X GET "$base_url$dns_zone" -H "$curl_param $api_key" -s | jq '.[] | .id?');
 		echo "zone_id=$zone_id" >> .env
 		. .env
 	fi
 }
 
 function checkIfRecordIdExists {
+
 	if [ -z ${record_id+x} ]; 
 	then
-		echo "URL"
-		echo "$base_url$dns_zone/$zone_id?recordName=$domain&recordType=$dns_type"
 		# get record ID
-		record_id=$(curl -X GET "$base_url$dns_zone/$zone_id?recordName=$domain&recordType=$dns_type" -H $output_type -H "$curl_param $api_key" | jq '.records? | .[] | .id?')
+		record_id=$(curl -X GET "$base_url$dns_zone/$zone_id?recordName=$domain&recordType=$dns_type" -H $output_type -H "$curl_param $api_key" -s | jq '.records? | .[] | .id?')
 		echo "record_id=$record_id" >> .env
 		. .env
 	fi
 }
 
+function GetDNSRecord {
+
+	record_url="$base_url$dns_records_start$zone$zone_id$dns_records_end$record_id"
+
+	curl -X GET "$record_url" -H "$output_type" -H "$curl_param $api_key" -s | jq '.content'
+}
+
+function UpdateDNSRecord {
+
+	updater_url="$base_url$dns_records_start$zone$zone_id$dns_records_end$record_id"
+	req_body="{\"content\":\"$1\"}"
+
+	curl -X PUT "$updater_url" -H "Content-Type: application/json" -H "$curl_param $api_key" -d "$req_body"
+}
+
 checkIfZoneIdExists
 checkIfRecordIdExists
 
-# curl -X GET "$base_url$dns_records_start/$zone_id/$dns_records_end/$record_id" -H $output_type -H "$curl_param $api_key" -v | jq 
+# get current ip in dns record
+current_ip=$(GetDNSRecord)
+
+# update ip if they don't match
+if [[ "$1" != "$current_ip" ]];
+then
+	UpdateDNSRecord "$1"
+fi
