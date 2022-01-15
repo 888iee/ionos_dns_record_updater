ecab8bf1af6e8afa67d9fd43984dfe537a83ac15
devDath <sidathwayne@aol.com>
2022-01-15 15:58:20 +0100

    matching records will get deleted now

--- updater.sh
+++ updater.sh
@@ -36,13 +36,13 @@ function Help() {
 
 function RetrieveIpAdress() {
 	ip=$(curl -s https://ipinfo.io/ip)
-	echo "ip set to $ip" 
+	# echo "ip set to $ip" 
 }
 
 function RetrieveZoneId() {
 
 	# get zone ID
-	zone_id=$(curl -X GET "$base_url$dns_zone" -H "$curl_param $api_key" );
+	zone_id=$(curl -X GET "$base_url$dns_zone" -H "$curl_param $api_key" -s );
 	
 	# check if valid object was found
 	name=$(echo $zone_id | jq '.[] | .name?' );
@@ -54,17 +54,41 @@ function RetrieveZoneId() {
 		exit 2
 	fi
 
-	zone_id=$(echo $zone_id | jq '.[] | .id?');
+	zone_id=$(echo $zone_id | jq '.[] | .id?' | tr -d '"');
 }
 
+function DeleteRecord() {
+	delete_url="$base_url$dns_zone/$zone_id/records/$1"
+
+	echo $1
+	curl -X DELETE $delete_url -H "accept: */*" -H "$curl_param $api_key"
+}
+
+function GetCustomerZone() {
+	customer_url="$base_url$dns_zone/$zone_id?recordType=$dns_type"
+	
+	records=$(curl -X GET $customer_url -H $output_type -H "$curl_param $api_key" -s | jq '.records')
+	# echo $bla
+	echo $records | jq -c '.[]'  | while read i; do
+
+		name=$(echo $i | jq '.name' | tr -d '"')
+		
+		if [[ $name = "$domain" || $name = "www.$domain" ]];
+		then
+			rec_id=$(echo $i | jq '.id' | tr -d '"')
+			DeleteRecord "$rec_id"
+		fi
+	done
+}
+	
 function CheckIP() {
 	# check ip regex
 	if [[ $ip =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}$ ]];
 	then
 		echo "ip set to $ip" 
 	else
-		echo "Given adress is not a valid ip. 
-This script will search for the actual ip adress of this machine."
+# 		echo "adress isn't valid or set. 
+# This script will search for the actual ip adress of this machine."
 		RetrieveIpAdress
 	fi
 }
@@ -95,41 +119,4 @@ done
 # checks if ip was set and retrieves it if not
 CheckIP
 RetrieveZoneId
-
-function checkIfRecordIdExists {
-
-	if [ -z ${record_id+x} ]; 
-	then
-		# get record ID
-		record_id=$(curl -X GET "$base_url$dns_zone/$zone_id?recordName=$domain&recordType=$dns_type" -H $output_type -H "$curl_param $api_key" -s | jq '.records? | .[] | .id?')
-		echo "record_id=$record_id" >> "$SCRIPTPATH/.env"
-		. "$SCRIPTPATH/.env"
-	fi
-}
-
-function GetDNSRecord {
-
-	record_url="$base_url$dns_records_start$zone$zone_id$dns_records_end$record_id"
-
-	curl -X GET "$record_url" -H "$output_type" -H "$curl_param $api_key" -s | jq '.content'
-}
-
-function UpdateDNSRecord {
-
-	updater_url="$base_url$dns_records_start$zone$zone_id$dns_records_end$record_id"
-	req_body="{\"content\":\"$1\"}"
-
-	curl -X PUT "$updater_url" -H "Content-Type: application/json" -H "$curl_param $api_key" -d "$req_body"
-}
-
-# checkIfZoneIdExists
-# checkIfRecordIdExists
-
-# # get current ip in dns record
-# current_ip=$(GetDNSRecord)
-
-# # update ip if they don't match
-# if [[ "$1" != "$current_ip" ]];
-# then
-# 	UpdateDNSRecord "$1"
-# fi
+GetCustomerZone
